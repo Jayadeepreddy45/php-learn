2d35262ccc811d55241c6a008b9b348a896f5381
Jayadeepreddy45 <Jayadeepreddy452002@gmail.com>
2024-11-20 22:58:48 +0530

    comp

---
+++ comparision.php
@@ -0,0 +1,19 @@
+// Comparison Operators
+<?php
+    // echo "<h1> Comparison Operators </h1>";
+    echo "The value of 1==4 is ";
+    echo var_dump(1==4);
+    echo "<br>";
+
+    echo "The value of 1!=4 is ";
+    echo var_dump(1!=4);
+    echo "<br>";
+
+    echo "The value of 1>=4 is ";
+    echo var_dump(1>=4);
+    echo "<br>";
+
+    echo "The value of 1<=4 is ";
+    echo var_dump(1<=4);
+    echo "<br>";
+    ?>
